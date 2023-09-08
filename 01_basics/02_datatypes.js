7bb1a3745e40fa514afd68b98127f7809629b47e
gatlagangothri <139121392+gatlagangothri@users.noreply.github.com>
2023-09-08 11:17:43 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+//console.log(5 + 4)
+//console.log("Gayathri")
+
+let name = "gayathri"
+let age = 22
+let isLoggedIn = false
+
+console.log(typeof "gayathri");
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
