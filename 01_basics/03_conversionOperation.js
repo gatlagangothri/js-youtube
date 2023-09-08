5721d11a5b2728624fb90b5fb41062c6c918f24c
gatlagangothri <139121392+gatlagangothri@users.noreply.github.com>
2023-09-08 11:35:59 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+//let isLoggedIn = 1
+//let isLoggedIn = ""
+let isLoggedIn = "gayathri"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//"" => false
+//"gayathri" => true
+
+let someNumber = 54
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
