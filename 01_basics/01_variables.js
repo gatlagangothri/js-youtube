11cd2ae3ea3443b1a7659a12abaa315fdace0732
gatlagangothri <139121392+gatlagangothri@users.noreply.github.com>
2023-09-08 11:04:00 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 445589
+let accountEmail = "gayathri@google.com"
+var accountPassword = "54321"
+accountCity = "Hyderabad"
+
+
+accountEmail = "gr@gr.com"
+accountPassword = "4458"
+accountCity = "Bangalore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
